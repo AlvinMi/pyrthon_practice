fd6fc812c447d8318ac0bced6c7f6bc7816d80b0
AlvinMi <alvin.mi0620@gmail.com>
2020-02-29 23:28:50 +0800

    web automation

---
+++ 01/name-mean.py
@@ -0,0 +1,11 @@
+from selenium  import webdriver
+# 打开浏览器
+driver = webdriver.Chrome()
+driver.get("https://www.baidu.com")
+
+baidu_input = driver.find_element_by_id('kw')
+baidu_input.clear()
+baidu_input.send_keys('hello world')
+
+baidu_button = driver.find_element_by_id('su')
+baidu_button.click()
